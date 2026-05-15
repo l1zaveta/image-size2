37c88724736bf1556b166ec609fee1b10ec07f3c
Elizaveta Krasnoperova <144271794+l1zaveta@users.noreply.github.com>
2026-05-15 18:48:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,28 +5,31 @@ const sharp = require('sharp');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.use(express.urlencoded({ extended: true }));
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-
-app.use((req, res, next) => {
-    res.type('text/plain');
-    next();
-});
-
-
 app.get('/', (req, res) => {
+    res.type('text/plain');
     res.send('OK');
 });
 
-
 app.get('/login/', (req, res) => {
+    res.type('text/plain');
     res.send('l1zavetkns');
 });
-
 
 app.post('/size2json/', upload.single('image'), async (req, res) => {
     if (!req.file) {
@@ -39,7 +42,6 @@ app.post('/size2json/', upload.single('image'), async (req, res) => {
         return res.json({ width: 0, height: 0 });
     }
 });
-
 
 app.post('/insert/', async (req, res) => {
     const { login, password, URL } = req.body;
@@ -65,6 +67,7 @@ app.post('/insert/', async (req, res) => {
 
         await mongoose.disconnect();
 
+        res.type('text/plain');
         res.send('Document inserted');
     } catch (error) {
         console.error(error.message);
