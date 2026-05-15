8b3f413e7da008454143705a9c64ffd874d94a65
Elizaveta Krasnoperova <144271794+l1zaveta@users.noreply.github.com>
2026-05-15 18:53:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', '*');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
